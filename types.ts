d48ff8bd493948c87abe52fc1eaaa72c404490c8
Qiao Han <qiao@supabase.io>
2022-08-05 21:42:16 +0800

    Update base type

--- types.ts
+++ types.ts
@@ -8,57 +8,7 @@ export type Json =
 
 export interface Database {
   public: {
-    Tables: {
-      order: {
-        Row: {
-          id: number;
-          created_at: string;
-          quantity: number;
-          complete: boolean;
-          pet_id: number | null;
-          status: "CREATED" | "PAID" | "DELIVERED";
-        };
-        Insert: {
-          id?: number;
-          created_at?: string;
-          quantity: number;
-          complete?: boolean;
-          pet_id?: number | null;
-          status?: "CREATED" | "PAID" | "DELIVERED";
-        };
-        Update: {
-          id?: number;
-          created_at?: string;
-          quantity?: number;
-          complete?: boolean;
-          pet_id?: number | null;
-          status?: "CREATED" | "PAID" | "DELIVERED";
-        };
-      };
-      pet: {
-        Row: {
-          id: number;
-          created_at: string;
-          name: string;
-          tags: string[];
-          meta: Json | null;
-        };
-        Insert: {
-          id?: number;
-          created_at?: string;
-          name: string;
-          tags: string[];
-          meta?: Json | null;
-        };
-        Update: {
-          id?: number;
-          created_at?: string;
-          name?: string;
-          tags?: string[];
-          meta?: Json | null;
-        };
-      };
-    };
+    Tables: {};
     Functions: {};
   };
 }
