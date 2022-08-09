16105f71814c3b5c5ac4db0714dbb8318307d9db
Han Qiao <qiao@supabase.io>
2022-08-09 22:09:07 +0800

    Add new tables should trigger ci (#2)
    Initial remote commit

--- types.ts
+++ types.ts
@@ -8,7 +8,57 @@ export type Json =
 
 export interface Database {
   public: {
-    Tables: {};
+    Tables: {
+      order: {
+        Row: {
+          quantity: number;
+          pet_id: number | null;
+          created_at: string;
+          complete: boolean;
+          status: "CREATED" | "PAID" | "DELIVERED";
+          id: number;
+        };
+        Insert: {
+          quantity: number;
+          pet_id?: number | null;
+          created_at?: string;
+          complete?: boolean;
+          status?: "CREATED" | "PAID" | "DELIVERED";
+          id?: number;
+        };
+        Update: {
+          quantity?: number;
+          pet_id?: number | null;
+          created_at?: string;
+          complete?: boolean;
+          status?: "CREATED" | "PAID" | "DELIVERED";
+          id?: number;
+        };
+      };
+      pet: {
+        Row: {
+          name: string;
+          tags: string[];
+          meta: Json | null;
+          created_at: string;
+          id: number;
+        };
+        Insert: {
+          name: string;
+          tags: string[];
+          meta?: Json | null;
+          created_at?: string;
+          id?: number;
+        };
+        Update: {
+          name?: string;
+          tags?: string[];
+          meta?: Json | null;
+          created_at?: string;
+          id?: number;
+        };
+      };
+    };
     Functions: {};
   };
 }
